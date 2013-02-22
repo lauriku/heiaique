69b4a26ee061a04f9a84aac8628dd1733d0668d5
lauriku <lauriku@gmail.com>
2013-02-22 07:57:41 +0200

    Load config file no matter where the script is run from

--- heiaique/main.py
+++ heiaique/main.py
@@ -1,12 +1,16 @@
+import os
+
 from lib import HeiaClient
 from lib import XMLParser
 
 if __name__ == '__main__':
   client = HeiaClient()
 
-  if client.read_config():
+  main_path = os.path.dirname(os.path.abspath(__file__))
+  print main_path
+  config_file = os.path.join(main_path, "config.cfg")
+  print config_file
+  if client.read_config(config_file):
     print client.list_sports()
   else:
     print "Config file not found!"
-
-
