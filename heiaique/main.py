bec0c7d632ba2200a5ed835e13497ae27ee5d904
lauriku <lauriku@gmail.com>
2013-02-22 08:04:17 +0200

    Remove debugging lines

--- heiaique/main.py
+++ heiaique/main.py
@@ -7,9 +7,8 @@ if __name__ == '__main__':
   client = HeiaClient()
 
   main_path = os.path.dirname(os.path.abspath(__file__))
-  print main_path
   config_file = os.path.join(main_path, "config.cfg")
-  print config_file
+
   if client.read_config(config_file):
     print client.list_sports()
   else:
