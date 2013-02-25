0bb16f15586281d86ce98852221a4a7ca565ba00
Lauri Kuittinen <lauriku@gmail.com>
2013-02-25 19:25:22 +0200

    Try out command line options

--- heiaique/main.py
+++ heiaique/main.py
@@ -1,15 +1,20 @@
 import os
+import sys
 
 from lib import HeiaClient
 from lib import XMLParser
+from lib import Options
 
 if __name__ == '__main__':
+  options = Options()
+  opts, args = options.parse(sys.argv[1:])
+
   client = HeiaClient()
 
   main_path = os.path.dirname(os.path.abspath(__file__))
   config_file = os.path.join(main_path, "config.cfg")
-
-  if client.read_config(config_file):
-    print client.list_sports()
-  else:
-    print "Config file not found!"
+  if opts.sport_list:
+    if client.read_config(config_file):
+      print client.list_sports()
+    else:
+      print "Config file not found!"
