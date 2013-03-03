833b63d51e284b0d9e65b46b41bab78444e2211f
Lauri Kuittinen <lauriku@gmail.com>
2013-03-03 16:28:42 +0200

    Add some command line options

--- heiaique/lib/options.py
+++ heiaique/lib/options.py
@@ -8,7 +8,7 @@ class Options:
     def _init_parser(self):
         usage = 'bin/heaique'
         self.parser = OptionParser(usage=usage)
-        self.parser.add_option("-l", "--list", action="store_true", dest="sport_list", help="List sports", default=False)
+        self.parser.add_option("-l", "--list", action="store_true", dest="list_sports", help="List sports", default=False)
         self.parser.add_option("-g", "--get-training-logs", action="store_true", dest="get_training_logs", help="Get training logs", default=False)
         self.parser.add_option("-d", "--date", dest="date", help="Date, format YYYY-MM-DD")
         self.parser.add_option("-y", "--year", dest="year", help="Year, format YYYY")
