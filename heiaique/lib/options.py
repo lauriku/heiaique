ff4dd46fc7a3840e467e28732d247b60cbe981f9
Lauri Kuittinen <lauriku@gmail.com>
2013-03-03 16:12:46 +0200

    Add a few opts

--- heiaique/lib/options.py
+++ heiaique/lib/options.py
@@ -9,6 +9,9 @@ class Options:
         usage = 'bin/heaique'
         self.parser = OptionParser(usage=usage)
         self.parser.add_option("-l", "--list", action="store_true", dest="sport_list", help="List sports", default=False)
+        self.parser.add_option("-g", "--get-training-logs", action="store_true", dest="get_training_logs", help="Get training logs", default=False)
+        self.parser.add_option("-d", "--date", dest="date", help="Date, format YYYY-MM-DD")
+        self.parser.add_option("-y", "--year", dest="year", help="Year, format YYYY")
 
     def parse(self, args = None):
         return self.parser.parse_args(args)
