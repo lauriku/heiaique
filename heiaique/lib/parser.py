4c89ed819c35917a292ab1ac09798464d2b1ea30
Lauri Kuittinen <lauriku@gmail.com>
2013-03-09 09:05:20 +0200

    Add a method to iterate over the training log entries, and return a list of dictionaries

--- heiaique/lib/parser.py
+++ heiaique/lib/parser.py
@@ -1,15 +1,25 @@
 from lxml import etree
+from io   import BytesIO
 
 class Parser:
 
   def __init__(self):
     pass
 
-  def parse(self, data):
-    try:
-      etree.parse(data)
-    except etree.XMLSyntaxError, e:
-      pass # just put the exception into e
+  def parse_training_logs(self, data):
+    file_like_object = BytesIO(data)
+    context = etree.iterparse(file_like_object)
+    training_log = {}
+    training_log_entries = []
+    for action, elem in context:
+      if not elem.text:
+        text = "None"
+      else:
+        text = elem.text
+      training_log[elem.tag] = text
+      if elem.tag == "training-log":
+        training_log_entries.append(training_log)
+    return training_log_entries
 
 
 
