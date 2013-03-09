1574e46fdc76c5c3b57c5cfb20d957aa7a69b0ea
Lauri Kuittinen <lauriku@gmail.com>
2013-03-09 12:22:56 +0200

    Add a few parsing methods

--- heiaique/lib/parser.py
+++ heiaique/lib/parser.py
@@ -6,14 +6,45 @@ class Parser:
   def __init__(self):
     pass
 
+  def parse_sport_list(self, data):
+    file_like_object = BytesIO(data)
+    context = etree.iterparse(file_like_object)
+    sport = {}
+    sportlist = []
+    for action, elem in context:
+      if not elem.text or elem.text.isspace():
+        text = None
+      else:
+        text = elem.text
+        sport[elem.tag] = text
+      if elem.tag == "sport":
+        sportlist.append(sport)
+        sport = {}
+    return sportlist
+
+  def parse_single_sport(self, data):
+    file_like_object = BytesIO(data)
+    context = etree.iterparse(file_like_object)
+    sport = {}
+    for action, elem in context:
+      if not elem.text or elem.text.isspace():
+        text = None
+      else:
+        text = elem.text
+        sport[elem.tag] = text
+    return sport
+
   def parse_training_logs(self, data):
+    """Parse the xml from HeiaHeia into a list of training entries, 
+    each training entry as a dictionary"""
+
     file_like_object = BytesIO(data)
     context = etree.iterparse(file_like_object)
     training_log = {}
     training_log_entries = []
     for action, elem in context:
-      if not elem.text:
-        text = "None"
+      if not elem.text or elem.text.isspace():
+        text = None
       else:
         text = elem.text
       training_log[elem.tag] = text
